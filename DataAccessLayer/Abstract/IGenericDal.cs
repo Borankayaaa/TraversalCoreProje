f5b24592b0ca1f714d748c1078f7e3ffe8d6a280
Boran Kaya <borankaya210@gmail.com>
2024-07-16 15:54:12 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> // new Item oluşturarak uzun uzun hepsine crud işlemleri yazmak yerine Generic sınıfı kullanarak tüm sınıflara miras vereceğiz...
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+
+    }
+}
---
+++ EntityLayer/Concrete/About2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About2 // En Alta olan 3 kısımlı yer burayı About2 olarak almayı tercih ettim
+    {
+        [Key]
+        public int About2ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
