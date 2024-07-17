7ebf7f585f3ee0288660f93edd7280d819dde71b
Boran Kaya <borankaya210@gmail.com>
2024-07-17 17:57:31 +0300

    ViewComponentler yapmaya Başlandı devamı gelecek....

--- BusinessLayer/ValidationRules/AboutValidator.cs
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules
 {
-    internal class AboutValidator
+    public class AboutValidator : AbstractValidator<About>
     {
+        public AboutValidator() //RuleFor() metodu ile mesaj gönderebilriiz. //validation'ı UI katmanında kullanmak için oraya da paketi indirmeliyiz..
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama kısmını Boş Geçemezsiniz...!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Görsel Seçiniz...!");
+            RuleFor(X => X.Description).MinimumLength(50).WithMessage("Lütfen en az 50 karakterlik açıklama bilgisi giriniz...!");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Karakter sınırını geçtiniz maksimum 1500 karakter.");
+        }
     }
 }
