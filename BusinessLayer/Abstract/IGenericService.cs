43ac16e5e85d593448d91220a977e060374d6619
Boran Kaya <borankaya210@gmail.com>
2024-07-17 12:09:17 +0300

    Abstract EF bağlantıları yapıldı Devam edecek...

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T> // Business'e gelmeden önce business tarafından tanımlanacak  bütün metodlar buradan geçip business ve concrete ye geçecek
+    {
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> GetList();
+        T GetById(int id);
+
+
+
+
+    }
+}
---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal   aboutDal) //Bu Yapının Adı dependency injection
+        {
+                _aboutDal = aboutDal;       //Bu sayede DataAccess içerisindeki verilere erişim sağlayabildik //_aboutDal'ı çağırdığımda crud işlemlerini almalıyım amacım bu(IGenericDal'ın içerisinde olanlar)
+        }
+        public About GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetList()
+        {
+           return _aboutDal.GetList();
+        }
+
+        public void TAdd(About t)
+        {
+           _aboutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+           _aboutDal.Delete(t);
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
