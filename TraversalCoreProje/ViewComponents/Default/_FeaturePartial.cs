288d65a0213022d4191d16a6b56b199e55171cf9
Boran Kaya <borankaya210@gmail.com>
2024-07-20 23:51:49 +0300

    Feature bölümü bitti.

---
+++ TraversalCoreProje/ViewComponents/Default/_FeaturePartial.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _FeaturePartial : ViewComponent
+    {
+         FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+
+            var values = featureManager.GetList();
+            
+            return View(values);
+        }
+    }
+}
