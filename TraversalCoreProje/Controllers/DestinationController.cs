b3b3840e5f93e0d046f11ab5c2a78f21aa0bf732
Boran Kaya <borankaya210@gmail.com>
2024-07-23 17:48:15 +0300

    ...

---
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(DestinationController p)
+        {
+            return View();
+        }
+    }
+}
